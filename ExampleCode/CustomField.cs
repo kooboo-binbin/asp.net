5bb67b95542e50c0378ff180d33af8106477c974
Ruben Verschueren <rubenverschueren@hotmail.com>
2015-10-23 18:23:46 +0200

    added custom data
    how to add custom data via JSON encoded field

---
+++ ExampleCode/CustomField.cs
@@ -0,0 +1,12 @@
+﻿public class CustomField
+{
+    public CustomField(string name, string value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    public string Name { get; set; }
+
+    public string Value { get; set; }
+}
---
+++ ExampleCode/JsonEncoder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class JsonEncoder
+{
+    public List<CustomField> Data { get; set; } 
+    
+    public JsonEncoder(List<CustomField> data )
+    {
+        Data = data;
+    }
+
+    public string GetJson()
+    {
+        return JsonConvert.SerializeObject(Data);
+    }
+}
