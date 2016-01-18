42e63759a1c03f4bfbd4009ee457f8af13eb41f9
binbin <binbin@kooboo.com>
2016-01-18 11:50:46 +0800

    add callback and return pages

---
+++ AdyenExamples/Callback.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Callback : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var path = Server.MapPath("~/App_data/log.txt");
+        using (var sw = new StreamWriter(path, true))
+        {
+            var message = DateTime.Now + " " + Request.UserHostAddress + "\r\n" + Request.QueryString.ToString() + "\r\n" + Request.Form.ToString();
+            sw.Write(message);
+        }
+    }
+}
---
+++ AdyenExamples/Return.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Return.aspx.cs" Inherits="Return" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            I am pay result page + <%=DateTime.Now %>
+        </div>
+    </form>
+</body>
+</html>
